d6af3e41f130eb498a449022e27954ee12ab7bb3
Çınar Göksu <126814579+cnrgks@users.noreply.github.com>
2023-05-30 10:09:32 +0300

    Add files via upload

---
+++ lib/screens/welcome.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [BottomNavigationBar].
+
+void main() => runApp(const Welcome());
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomNavigationBarExample(),
+    );
+  }
+}
+
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
+
+  @override
+  State<BottomNavigationBarExample> createState() =>
+      _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+      static const List<String> _pageTitles = [
+    'Anasayfa',
+    'Kurumsal',
+    'İletişim',
+    'Ayarlar',
+  ];
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Anasayfa',
+      style: optionStyle,
+    ),
+    Text(
+      'Kurumsal',
+      style: optionStyle,
+    ),
+    Text(
+      'İletişim',
+      style: optionStyle,
+    ),
+    Text(
+      'Ayarlar',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pageTitles[_selectedIndex]),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.grey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Anasayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business_center),
+            label: 'Kurumsal',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.phone),
+            label: 'İletişim',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Ayarlar',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+      
+    );
+  }
+}
